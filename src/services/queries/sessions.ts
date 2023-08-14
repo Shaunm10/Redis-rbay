eed7c128fc2c08e5bae20b5dae0a48a2d819a5d0
Shaunm10 <shaunm10@gmail.com>
2023-08-14 16:48:42 -0400

    Added Session work.

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,35 @@
+import { sessionKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { Session } from '$services/types';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionKey(id));
 
-export const saveSession = async (session: Session) => {};
+	// determine if this is an empty object and there is no user hash
+	if (!Object.keys(session).length) {
+		// if we don't have an properties, return null to signify there is no session
+		return null;
+	}
+
+	console.log(session);
+	return deserialize(id, session);
+};
+
+export const saveSession = async (session: Session) => {
+	return await client.hSet(sessionKey(session.id), serialize(session));
+};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+	return {
+		id,
+		userId: session.userId,
+		username: session.username
+	};
+};
+
+const serialize = (session: Session) => {
+	return {
+		userId: session.userId,
+		username: session.username
+	};
+};
