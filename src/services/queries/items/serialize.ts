8ba2e3f018da341351aeb306a71b71851097c8d0
shaunsargent <ShaunM10@gmail.com>
2023-08-14 20:46:46 -0400

    Item WIP

--- src/services/queries/items/serialize.ts
+++ src/services/queries/items/serialize.ts
@@ -1,3 +1,15 @@
 import type { CreateItemAttrs } from '$services/types';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+/**
+ * Converts a View Model to an object to be stored in Redis
+ * @param attrs 
+ * @returns
+ */
+export const serialize = (attrs: CreateItemAttrs) => {
+
+    return {
+        ...attrs,
+        createdAt: attrs.createdAt.toMillis(),
+        endingAt: attrs.endingAt.toMillis()
+    };
+};
