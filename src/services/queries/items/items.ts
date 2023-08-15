78f1467ea248ece71c2fd1baf0032956a9c1720d
shaunsargent <ShaunM10@gmail.com>
2023-08-14 20:28:48 -0400

    Added comments to the Items.ts (repo)

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,20 @@
 import type { CreateItemAttrs } from '$services/types';
 
-export const getItem = async (id: string) => {};
+/**
+ * Get's single item.
+ * @param id The Id of the item.
+ */
+export const getItem = async (id: string) => { };
 
-export const getItems = async (ids: string[]) => {};
+/**
+ * Gets multiple Items
+ * @param ids the Id's of the items to retrieve.
+ */
+export const getItems = async (ids: string[]) => { };
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+/**
+ * Creates a brand new item.
+ * @param attrs the data about the item
+ * @param userId The user's Id who is creating the item
+ */
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => { };
