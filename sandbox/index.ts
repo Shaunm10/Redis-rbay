cfe21f6fdfff6616cc4c60696f49224995a890bd
Shaunm10 <shaunm10@gmail.com>
2023-08-18 14:10:44 -0400

    Added example of dispatching multiple gets in Redis at once.

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {
+const runBasicCommands = async () => {
 	await client.hSet('car', {
 		color: 'red',
 		year: 1950
@@ -11,4 +11,30 @@ const run = async () => {
 	console.log(car);
 	console.log(await client.PING());
 };
-run();
+
+const runPipelineBatchCommands = async () => {
+	// first create 3 different hashsets to fetch
+	await client.hSet('car1', {
+		color: 'red',
+		year: 1950
+	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
+
+	// now get them all at once.
+	const results = await Promise.all([
+		client.hGetAll('car1'),
+		client.hGetAll('car2'),
+		client.hGetAll('car3')
+	]);
+
+	console.log(results);
+};
+runPipelineBatchCommands();
+//runBasicCommands();
