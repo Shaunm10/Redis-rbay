f6e9ea75361a7a86b1b3d222df32225ffa0e1862
shaunsargent <ShaunM10@gmail.com>
2023-08-21 12:53:15 -0400

    Added abilty to like and unlike an item

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,16 +1,26 @@
 import { userLikesKey } from "$services/keys";
 import { client } from "$services/redis";
 
-export const userLikesItem = async (itemId: string, userId: string) => { };
+/** If a user has liked a particular item. */
+export const userLikesItem = async (itemId: string, userId: string) => {
+    // we are seeing if this item's Id is in their liked Set to determine if they like it.
+    const isItemLiked = await client.sIsMember(userLikesKey(userId), itemId);
+    return isItemLiked;
+};
 
 export const likedItems = async (userId: string) => { };
 
 /** Adds a liked item for a user. */
 export const likeItem = async (itemId: string, userId: string) => {
+
+    // Add's the itemId to a user's set list.
     await client.sAdd(userLikesKey(userId), itemId);
 };
 
+/** Removes a liked item for a user. */
 export const unlikeItem = async (itemId: string, userId: string) => {
+
+    // Remove's the itemId to a user's set list.
     await client.sRem(userLikesKey(userId), itemId);
 };
 
