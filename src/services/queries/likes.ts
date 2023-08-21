f1487877f0caa924c61875dcea9e7a85782790ed
shaunsargent <ShaunM10@gmail.com>
2023-08-21 13:23:49 -0400

    Working on likeing and unliking but view is not updating.

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,6 +1,7 @@
-import { userLikesKey } from "$services/keys";
+import { itemsKey, userLikesKey } from "$services/keys";
 import { client } from "$services/redis";
 
+const likes = 'likes';
 /** If a user has liked a particular item. */
 export const userLikesItem = async (itemId: string, userId: string) => {
     // we are seeing if this item's Id is in their liked Set to determine if they like it.
@@ -14,14 +15,24 @@ export const likedItems = async (userId: string) => { };
 export const likeItem = async (itemId: string, userId: string) => {
 
     // Add's the itemId to a user's set list.
-    await client.sAdd(userLikesKey(userId), itemId);
+    const isValueInserted = await client.sAdd(userLikesKey(userId), itemId);
+
+    // also increment the product's like count.
+    if (isValueInserted) {
+        return client.hIncrBy(itemsKey(itemId), likes, 1);
+    }
 };
 
 /** Removes a liked item for a user. */
 export const unlikeItem = async (itemId: string, userId: string) => {
 
     // Remove's the itemId to a user's set list.
-    await client.sRem(userLikesKey(userId), itemId);
+    const removed = await client.sRem(userLikesKey(userId), itemId);
+
+    // also decrement the product's like count.
+    if (removed) {
+        return client.hIncrBy(itemsKey(itemId), likes, -1);
+    }
 };
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => { };
