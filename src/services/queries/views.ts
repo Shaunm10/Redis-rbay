cf4c6614f8e510dba638f8ed7514a8ab2ad4a040
shaunsargent <ShaunM10@gmail.com>
2023-08-26 21:47:12 -0400

    - added functionality to save view counts.

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,1 +1,17 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { itemsByViewKey, itemsKey } from "$services/keys";
+import { client } from "$services/redis";
+
+const ViewsPropertyName = 'views';
+
+export const incrementView = async (itemId: string, userId: string) => {
+
+    // do both actions in parallel
+    await Promise.all([
+
+        // update the hash set view count.
+        client.hIncrBy(itemsKey(itemId), ViewsPropertyName, 1),
+
+        // update the sorted set's view.
+        client.zIncrBy(itemsByViewKey(), 1, itemId)
+    ]);
+};
