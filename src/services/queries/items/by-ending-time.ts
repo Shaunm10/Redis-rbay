471f874e69ffcda7506d79d26d9f951e6002b139
Shaunm10 <shaunm10@gmail.com>
2023-08-28 17:05:30 -0400

    -WIP

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,5 @@
-export const itemsByEndingTime = async (
-	order: 'DESC' | 'ASC' = 'DESC',
-	offset = 0,
-	count = 10
-) => {};
+import { client } from '$services/redis';
+
+export const itemsByEndingTime = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	//const itemIds = await client.zRange(itemsByEndingAtKey,)
+};
