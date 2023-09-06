7eee188c57e6b0808803e962d868a4de8d60621f
Shaunm10 <shaunm10@gmail.com>
2023-09-06 17:22:57 -0400

    - aded minor comment.s

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,14 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { itemsByViewKey, itemsKey } from '$services/keys';
+import { client } from '$services/redis';
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	const results = await client.sort(itemsByViewKey(), {
+		// the projections of the sort
+		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+
+		// how the data should be sorted in the itemsByView
+		BY: 'score'
+	});
+
+	console.log(results);
+};
