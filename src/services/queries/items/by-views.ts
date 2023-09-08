57f9218642f0a4c8f5684ce77fc524492628bc9b
Shaunm10 <shaunm10@gmail.com>
2023-09-08 16:16:41 -0400

    Finished the itemsByViews method

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,14 +1,43 @@
 import { itemsByViewKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
+import { deserialize } from './deserialize';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-	const results = await client.sort(itemsByViewKey(), {
+	let results: any = await client.sort(itemsByViewKey(), {
 		// the projections of the sort
-		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
 
 		// how the data should be sorted in the itemsByView
-		BY: 'score'
+		BY: 'score',
+
+		DIRECTION: order,
+		LIMIT: {
+			offset,
+			count
+		}
 	});
 
-	console.log(results);
+	const items = [];
+	while (results.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+		const item = deserialize(id, {
+			name,
+			views,
+			imageUrl,
+			endingAt,
+			price
+		});
+
+		items.push(item);
+		results = rest;
+	}
+
+	return items;
 };
