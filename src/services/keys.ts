003bf8d47b6599e4fed5ad2098eeca5e5e1a7df6
Shaunm10 <shaunm10@gmail.com>
2023-09-08 16:59:44 -0400

    - updated incrementView to make use of a Hyperloglog.

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -17,4 +17,10 @@ export const itemsByViewKey = () => 'items:views';
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const itemsByEndingAtKey = () => `items:endingAt`;
 
+/**
+ * The key for storing userId views by productId
+ * Used in a HyperLogLog
+ * @param itemId The productId
+ * @returns the key
+ */
 export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`;
