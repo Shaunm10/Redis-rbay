dcf54221095f4653c3710858021feb3ac42931a8
Shaunm10 <shaunm10@gmail.com>
2023-10-09 16:09:16 -0400

    - finished the typeahead search.

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,34 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { itemsIndexKey } from '$services/keys';
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+
+export const searchItems = async (term: string, size: number = 5) => {
+	if (!term) {
+		return [];
+	}
+	// gets executed when user types in search textbox.
+	const cleaned = term
+		.replaceAll(/[^a-zA-Z0-9]/g, '')
+		.trim()
+		.split(' ')
+		.map((word) => (word ? `%${word}%` : ''))
+		.join('');
+
+	// Look at cleaned and make sure it is valid
+	if (cleaned === '') {
+		return [];
+	}
+
+	// use the client to do an actual search
+	const results = await client.ft.search(itemsIndexKey(), cleaned, {
+		LIMIT: {
+			from: 0,
+			size
+		}
+	});
+
+	console.log(results);
+
+	// Deserialize and return the search results
+	return results.documents.map(({ id, value }) => deserialize(id, value as any));
+};
